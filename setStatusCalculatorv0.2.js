bfe79d8efb7a763e83d0d1d881a1da035499967f
taariq ghoord <taariqghoord@gmail.com>
2015-03-05 01:00:44 +0200

    added a few functions

--- setStatusCalculatorv0.2.js
+++ setStatusCalculatorv0.2.js
@@ -27,7 +27,90 @@ exports.setStatusCalculator=function(setStatusCalculatorRequest)
 
     setStatusCalculatorResult.ProfileAssessor=setStatusCalculatorRequest.ProfileAssessor;///setting the profile assessor to whatever has been passed
     setStatusCalculatorResult.BuzzSpace=setStatusCalculatorRequest.BuzzSpace;
+
+
     return setStatusCalculatorResult;
 
 }
 
+//This Assessor takes the # of posts a user has made and devides it by the total number of posts on the space
+function ThreadsDepthAssessor()
+{
+
+    return document.treeSizeSum/document.numTrees
+
+}
+
+///this default Assessor returns the number of posts a user has made
+function NumPostsAssessor(userId)
+{
+    return db.find({"_id": userId}).count();
+
+
+}
+
+//This Assessor returns the users level on the BuzzSpace as his/her assessment
+function RoleAssessor()
+{
+    return db.find({"_id": userId},{"_role":role});
+
+}
+
+function WeightedSumProfileAssessor()
+{
+
+}
+
+function AppraisalsAssessor()
+{
+    db.getSiblingDB(databaseName).getCollection(collectionName).find({"_id": userId}).snapshot().forEach(
+        function(document)
+        {
+            db.getSiblingDB(databaseName).getCollection(collectionName).update(
+                {_id: document._id}, {$set:{"statusPoints":  document.appraisalTotal}});
+        });
+
+}
+
+
+/*
+
+
+
+ function getStatusForProfile(getStatusForProfileRequest, callback)
+ {
+ Users.findOne({"user_id": getStatusForProfileRequest}, function(err, user)//result is stored in user
+ {
+ if(err)
+ {
+ console.log("ERR: " + err);
+ }
+ else
+ {
+ callback(JSON.stringify(user.status_value));//sending result to callback function in this case as a json string
+ }
+ });
+ }
+ function temp(callback)
+ {
+ Users.find({}, function(err, user)	//results are stored in user
+ {
+ if(err)
+ {
+ console.log("ERR: " + err);
+ }
+ else
+ {
+ user.forEach(function(auser)	//each record in user is used as auser
+ {
+ //do something with each individual record
+ callback(user);//sends the results you want to work with to the callback function
+ });
+ }
+ });
+ }
+
+ //how we will be calling the functions
+ getStatusForProfile(userID,               callback);	//this is how the function looks to you
+ Status.getStatusForProfile(request.query.userID, Status.tempFunc);	//this is how we call the function from our side
+ */
