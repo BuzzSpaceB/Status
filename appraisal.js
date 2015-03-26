eea49a89b417b65c87d1455be6d9a62bf38a5f4b
AndrewParkes <andrewparkesgunnell@gmail.com>
2015-03-26 09:47:04 +0200

    Corrected appraisal into the correct format

--- appraisal.js
+++ appraisal.js
@@ -1,77 +1,40 @@
-//<img src="Emoj/A.png" title="Appraisal" id="appraisalButton" onclick="popup();" height="50" width="50"><div id="popon"></div>
-
-var popupHide=true;
-var loadedAppraisals=false;
-var tbl = document.createElement("table");
-var pop = document.getElementById("popon");
-
-function getCurrentAppraisal()
-{
-	/* 
-	If you have previously voted on an appraisal it should be added on loading of page
-	*/
-	//pop.src="Emoj/"+type+".png";
-}
-
-function popup()
-{
-	if(popupHide)//display appraisals
-	{
-		if(!loadedAppraisals)
-		{
-			addAppraisals();
-		}
-		displayPopup();
-	}
-	else// hide appraisals
-	{
-		hidePopup();
-	}
-}
-
-function addAppraisals()//adds Appraisal types to the appraisals table
-{
-	tbl.id = "popTable";
-			
-	//get location of appraisalButton and set it to bottom right on popup
-	var element = document.getElementById('appraisalButton');
-	var position = element.getBoundingClientRect();
-	var x = position.left;
-	var y = position.top;
-	pop.style.top=y+50+"px";
-	pop.style.left=x+50+"px";
+/**
+ * Created by Andrew Parkes
+ */
+ 
+ /**
+ * The system will allow users to upvote posts via an appraisal.
+ * This is done by a user being linked to a appraisal which is linked to a post.
+ * This is done through a pluggable GetAppraisalTypes aswell as setAppraisal,
+ * each implementing the Appraisal interface
+ * @param getUser - get the current usersId
+ * @param getPost -  get 
+ * @param getAppraisal - 
+ */
+ 
+ function setAppraisalForPost(getCurrentUser, getCurrentPost, getAppraisal)
+ {
 	
-	//create tble of appraisals only 4*4 currently not dynamic
-	for ( var r = 0; r < 4; ++r )
-	{
-		var tr = tbl.insertRow(-1);
-		for ( var c = 0; c < 4; ++c )
-		{
-			var td1 = tr.insertCell(-1);
-			td1.innerHTML = "<img src='Emoj/"+((r*4)+c+1)+".png' title='Smiley face' alt=''height='30' width='30' onclick='updateAppraisal("+((r*4)+c+1)+");'>";
-		}
-	}
-	pop.appendChild(tbl);
-	loadedAppraisals=true;
-}
-
-function hidePopup()
-{
-	pop.style.display = "none";
-	tbl.style.display = "none";
-	popupHide=true;
-}
-
-function displayPopup()
-{
-	pop.style.display = "block";
-	tbl.style.display = "block";
-	popupHide=false;
-}
-
-function updateAppraisal(type)
-{
-	hidePopup();
-	var pop = document.getElementById("appraisalButton");
-	pop.src="Emoj/"+type+".png";
-}
+ }
+ 
+  /**
+ * Being able to get the Appraisal for later use.
+ * @param getAppraisalIdForRequest - get the needed appraisal Id
+ */
+ 
+ function getAppraisal(getAppraisalIdForRequest)
+ {   
+	return db.appraisal.find({appraisalId:getAppraisalIdForRequest});
+ }
+ 
+   /**
+ * Being able to get the Appraisal Typr for later use such as counting appraisals.
+ * @param getAppraisalIdForRequest - get the needed appraisal Id
+ */
+ 
+  function getAppraisalType(getAppraisalIdForRequest)
+ {   
+	var type;
+	return db.appraisal.find({appraisalId:getAppraisalIdForRequest, appraisalType:type});
+	return type;
+ }
