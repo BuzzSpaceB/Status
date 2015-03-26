e44cbe6d47bc32f74172bf5bf73e57fdfc14d15a
Prenolan Govender <u13102380@tuks.co.za>
2015-03-26 23:14:56 +0200

    createAppraisals updated according to feedback

--- createAppraisals/createAppraisalType.js
+++ createAppraisals/createAppraisalType.js
@@ -1,3 +1,8 @@
+var jsonAppraisalLevels;
+var jsonAppraisal;
+var appraisalLevels;
+var appraisal;
+
 /**
  * Convert textbox input into json strings for database insertion
  *
@@ -7,9 +12,10 @@
  */
 exports.createAppraisal=function(_name, _description){
 
-    var appraisal={name: _name, description: _description};
-    return JSON.stringify(appraisal);
-}
+    appraisal={name: _name, description: _description};
+    jsonAppraisal=JSON.stringify(appraisal);
+    return jsonAppraisal;
+};
 
 
 /**
@@ -21,66 +27,77 @@ exports.createAppraisal=function(_name, _description){
  * @return {String}
  */
 exports.createAppraisalLevels=function(_lvl1, _lvl2, _lvl3){
-    var appraisalLevels={level1: _lvl1, level2: _lvl2, level3:_lvl3};
-    return JSON.stringify(appraisalLevels);
-}
+    appraisalLevels={level1: _lvl1, level2: _lvl2, level3:_lvl3};
+    jsonAppraisalLevels=JSON.stringify(appraisalLevels);
+    return jsonAppraisalLevels;
+};
 
 
 /**
  * Convert image/icon to base64
  *
  * @param  input - An uploaded image from html
+ * @param {String} iconName - Name of the image being uploaded (ie. lvl1, notratedicon, etc)
  * @return {String} base64
  */
-exports.readNotRatedIcon=function(input) {
+exports.readNotRatedIcon=function(input, iconName) {
     if (input.files && input.files[0]) {
         var FR = new FileReader();
         FR.readAsDataURL(input.files[0]);
+
+        if (iconName=="notratedicon")
+        {
+            appraisal["notratedicon"]=FR;
+        }
+        else
+        {
+            if (iconName=="lvl1")
+            {
+                appraisalLevels["lvl1icon"]=FR;
+            }
+            else
+            {
+                if (iconName=="lvl2")
+                {
+                    appraisalLevels["lvl2icon"]=FR;
+                }
+                else
+                {
+                    if (iconName=="lvl3")
+                    {
+                        appraisalLevels["lvl3icon"]=FR;
+                    }
+                }
+            }
+        }
+
+        jsonAppraisal=JSON.stringify(appraisal);
+        jsonAppraisalLevels=JSON.stringify(appraisalLevels);
+
         return FR;
     }
-}
-
-
-/**
- * Convert image/icon to base64
- *
- * @param  input - An uploaded image from html
- * @return {String} base64
- */
-exports.readLevel1Icon=function(input) {
-    if (input.files && input.files[0]) {
-       var lvl1 = new FileReader();
-        lvl1.readAsDataURL(input.files[0]);
-        return lvl1;
-    }
-}
-
+};
 
 /**
  * Convert image/icon to base64
  *
- * @param  input - An uploaded image from html
- * @return {String} base64
+ * @param  {String} dbURL - url of the database
+ * @param  dbCollections - collections used
  */
-exports.readLevel2Icon=function(input) {
-    if (input.files && input.files[0]) {
-       var lvl2 = new FileReader();
-        lvl2.readAsDataURL(input.files[0]);
-        return lvl2;
-    }
-}
+exports.store=function(dbURL, dbCollections){
+    var db=require('mongojs').connect(dbURL, dbCollections);
 
+    db.appraisals.save(jsonAppraisal, function(err, saved){
+        if (err || !saved)
+            console.log("Appraisal not added");
+        else
+            console.log("Appraisal added");
+    });
 
-/**
- * Convert image/icon to base64
- *
- * @param  input - An uploaded image from html
- * @return {String} base64
- */
-exports.readLevel3Icon=function(input) {
-    if (input.files && input.files[0]) {
-       var lvl3 = new FileReader();
-        lvl3.readAsDataURL(input.files[0]);
-        return lvl3;
-    }
-}
+    db.appraisallevels.save(jsonAppraisalLevels, function(err, saved){
+        if (err || !saved)
+            console.log("Appraisal Levels not added");
+        else
+            console.log("Appraisal Levels added");
+    });
+};
