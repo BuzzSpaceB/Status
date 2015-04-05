a4c95debcbb17c7346d0718163c1ed83df5fea4c
Johann Marx <u12105202@tuks.co.za>
2015-04-05 21:43:12 +0200

    Fixed errors

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * Created by johannmarx on 15/04/05.
  */
 
-module.exports.assessProfile = require('./StatusB/assessProfile.js');
-module.exports.getAppraisalIdForRequest = require('./StatusB/getAppraisal.js');
-module.exports.getAllAppraisalsForVote = require('./StatusB/getAllAppraisalsForVote.js');
-module.exports.getStatusForProfile = require('./StatusB/getStatusForProfilev0.1.js');
-module.exports.setStatusCalculator = require('./StatusB/setStatusCalculatorv0.2.js');
+module.exports.assessProfile = require('./assessProfile.js');
+module.exports.getAppraisalIdForRequest = require('./getAppraisal.js');
+module.exports.getAllAppraisalsForVote = require('./getAllAppraisalsForVote.js');
+module.exports.getStatusForProfile = require('./getStatusForProfilev0.1.js');
+module.exports.setStatusCalculator = require('./setStatusCalculatorv0.2.js');
