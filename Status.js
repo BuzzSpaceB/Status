2486af3f9a1c411c862468d97cd92c0528e894ad
Lelethu <u13028023@tuks.co.za>
2015-04-06 12:52:37 +0200

    Updated funtions to include additional parameters

--- Status.js
+++ Status.js
@@ -4,67 +4,70 @@
 *
 * @param {string} user_id - A user's id
 *
+* @param {number} tempResult - Value to update status points
+*
 */
-exports.updateStatusPointsForProfile = function(user_id)
+exports.updateStatusPointsForProfile = function(user_id, tempResult)
 {
-   try
-   	{
-   		var tempResult = Status.setStatusCalculator();
-   		Users.findOne({"user_id": user_id}, function(err, user)
-   		{
-   			if(err)
-   			{
-   				console.log("ERR: " + err);
-   			}
-   			else
-   			{
-   				user.status_value = tempResult;
-   				user.save(function(err)
-   				{
-   					if(err)
-   						console.log("ERR: " + err);
-   				});
-   			}
-   		});
-   	}
-   	catch(e)
-   	{
-   		console.log("Status Calculator error: " + e);
-   	}
+	try
+	{
+		Users.findOne({"user_id": user_id}, function(err, user)
+		{
+			if(err)
+			{
+				console.log("ERR: " + err);
+			}
+			else
+			{
+				user.status_value = tempResult;
+				user.save(function(err)
+				{
+					if(err)
+						console.log("ERR: " + err);
+				});
+			}
+		});
+	}
+	catch(e)
+	{
+		console.log("Status Calculator error: " + e);
+	}
 }
 
 /**
 *
 * Updates the status points of all users
 *
+* @param {number} tempResult - Value to update status points
+*
 */
-exports.updateAllStatusPoints = function()
+
+exports.updateAllStatusPoints = function(user, tempResult)
 {
-   try
-   	{
-   		var tempResult = Status.setStatusCalculator();
-   		Users.find({}, function(err, user)
-   		{
-   			if(err)
-   			{
-   				console.log("ERR: " + err);
-   			}
-   			else
-   			{
-   				user.forEach(function(auser)
-   				{
-   					auser.status_value = tempResult;
-   					auser.save(function(err)
-   					{
-   						if(err)
-   							console.log("ERR: " + err);
-   					});
-   				});
-   			}
-   		});
-   	}
-   	catch(e)
-   	{
-   		console.log("Status Calculator error: " + e);
-   	}
+	try
+	{
+		Users.find({}, function(err, user)
+		{
+			if(err)
+			{
+				console.log("ERR: " + err);
+			}
+			else
+			{
+				user.forEach(function(auser)
+				{
+					auser.status_value = tempResult;
+					auser.save(function(err)
+					{
+						if(err)
+							console.log("ERR: " + err);
+					});
+				});
+			}
+		});
+	}
+	catch(e)
+	{
+		console.log("Status Calculator error: " + e);
+	}
 }
